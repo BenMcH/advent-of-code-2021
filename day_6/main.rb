507c40938c5b91c5a60a640c61371a67721672a2
Ben McHone <benjamin.mchone@gmail.com>
2021-12-06 18:44:24 -0600

    Day 6 part 2

--- day_6/main.rb
+++ day_6/main.rb
@@ -1,22 +1,26 @@
-input_fish = File.read('input.txt').split(',').map(&:to_i)
-days = 80
+fish_counter = File.read('input.txt').split(',').map(&:to_i).group_by(&:itself).map { |k, v| [k, v.count] }.to_h
 
-fish = input_fish.dup
+def set_fishes(fish_map, fish_index, amount)
+	fish_map[fish_index] ||= 0
+	fish_map[fish_index] += amount
+end
 
-for i in 0..(days - 1) do
-	new_fish = []
-	fish = fish.map do |x|
-		if x == 0
-			new_fish << 8
-			6
-		else
-			x - 1
+def simulate_lantern_fish_day(fish_counter, days_to_simulate)
+	new_fish_counter = {}
+
+	fish_counter.keys.each do |fish|
+		count = fish_counter[fish]
+
+		if fish > 0
+			set_fishes(new_fish_counter, fish - 1, count)
+		elsif fish == 0
+			set_fishes(new_fish_counter, 6, count)
+			set_fishes(new_fish_counter, 8, count)
 		end
 	end
 
-	fish.concat(new_fish)
+	return days_to_simulate == 1 ? new_fish_counter : simulate_lantern_fish_day(new_fish_counter, days_to_simulate - 1)
 end
 
-
-
-p fish.length
+p simulate_lantern_fish_day(fish_counter, 80).values.sum
+p simulate_lantern_fish_day(fish_counter, 250).values.sum
