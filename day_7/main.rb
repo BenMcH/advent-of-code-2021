5235b85ae7622b05a95212069bb8e43a3d5d1321
Ben McHone <benjamin.mchone@gmail.com>
2021-12-07 06:45:10 -0600

    simplification of day 7

--- day_7/main.rb
+++ day_7/main.rb
@@ -1,38 +1,30 @@
 input = File.read('input.txt').split(',').map(&:to_i)
 
-def fuel_used_to_align_subs(subs, index)
-	fuel = 0
-	subs.each do |sub|
-		fuel += (sub - index).abs
-	end
-	fuel
+def get_value(distance = 1, start = 1)
+	distance == 0 ? 0 : start + get_value(distance - 1, start + 1)
 end
 
-def fuel_used_to_align_subs_pt2(subs, index)
-	fuel = 0
-	subs.each do |sub|
-		moves = (sub - index).abs
-		x = 1
-		moves.times do
-			fuel += x
-			x += 1
-		end
-	end
-	fuel
-end
-
-min = input.min
-max = input.max
+min, max = input.minmax
 range = min..max
 
 results = range.map do |target|
-	fuel_used_to_align_subs(input, target)
-end
+	input.reduce(0) do |acc, sub|
+		acc + (sub - target).abs
+	end
+end.min
 
-p results.min
+p results
+
+pt_2_distance = []
 
 results = range.map do |target|
-	fuel_used_to_align_subs_pt2(input, target)
-end
+	input.reduce(0) do |acc, sub|
+		moves = (sub - target).abs
+		if !pt_2_distance[moves]
+			pt_2_distance[moves] = get_value(moves)
+		end
+		acc + pt_2_distance[moves]
+	end
+end.min
 
-p results.min
+p results
