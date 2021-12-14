fc0914d570ae38b9c8a18d3513f7206c0821492e
Ben McHone <benjamin.mchone@gmail.com>
2021-12-14 12:12:49 -0600

    Day 14 part 2

--- day_14/main.rb
+++ day_14/main.rb
@@ -6,11 +6,29 @@ def build_polymers(polymers)
 	end.to_h
 end
 
-def step(str, polymers)
-	str.each_cons(2).map.with_index do |a, i|
-		start_index = i > 0 ? 1 : 0
-		polymers[a.join][start_index..-1]
-	end.join
+def simulate_day(counts, polymers, count = 1)
+	new_counter = {}
+	counts.keys.each.with_index do |key, i|
+		polymer = polymers[key]
+
+		polymer.split('').each_cons(2).map do |a, b|
+			key2 = "#{a}#{b}"
+			new_counter[key2] = new_counter[key2].to_i + counts[key]
+		end
+
+	end
+
+	return count == 1 ? new_counter : simulate_day(new_counter, polymers, count - 1)
+end
+
+def count_letters(counts)
+	new_counts = {}
+
+	counts.keys.each do |key|
+		new_counts[key[0]] = new_counts[key[0]].to_i + counts[key]
+	end	
+
+	new_counts
 end
 
 def part_1(start, polymers)
@@ -18,11 +36,14 @@ def part_1(start, polymers)
 
 	polymers = build_polymers(polymers)
 
-	10.times do |x|
-		str = step(str, polymers).split('')
-	end
+	counts = str.each_cons(2).map(&:join).tally
 
-	min, max = str.tally.to_a.map(&:last).minmax
+	result = simulate_day(counts, polymers, 10)
+
+	result = count_letters(result)
+	result[str[-1]] += 1
+
+	min, max = result.to_a.map(&:last).minmax
 
 	max - min
 end
@@ -37,6 +58,20 @@ start, polymers = File.read('input.txt').split("\n\n")
 p part_1(start, polymers)
 
 def part_2(start, polymers)
+	str = start.split('')
+
+	polymers = build_polymers(polymers)
+
+	counts = str.each_cons(2).map(&:join).tally
+
+	result = simulate_day(counts, polymers, 40)
+
+	result = count_letters(result)
+	result[str[-1]] += 1
+
+	min, max = result.to_a.map(&:last).minmax
+
+	max - min
 end
 
 ans = part_2(ex_start, ex_polymers)
